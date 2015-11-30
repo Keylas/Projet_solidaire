dce772f054b199c4ac66595ce5d125d6172f9d07
iskandar0 <corentin.robin@supelec.fr>
2015-11-30 16:45:54 +0100

    edition de l'affichage du module adherent

--- siteWeb_solidaire/module_Adherent/admin.py
+++ siteWeb_solidaire/module_Adherent/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from module_Adherent.models import Adherent, Ordinateur
 
+class adminAdherent(admin.ModelAdmin):
+	list_display = ('nom', 'prenom', 'dateExpiration', 'estValide')
+	list_filter = ('estRezoman', 'estValide')
+	date_hierarchy = 'dateExpiration'
+	ordering = ('dateExpiration', )
+	search_fields = ('nom', 'prenom', 'mail', 'chambre')
+
+class adminOrdinateur(admin.ModelAdmin):
+	list_display = ('possesseur', 'adresseIP', 'adresseMAC')
+	search_fields = ('possesseur', 'adresseIP', 'adresseMAC')
+
 
 admin.site.register(Adherent)
 admin.site.register(Ordinateur)
