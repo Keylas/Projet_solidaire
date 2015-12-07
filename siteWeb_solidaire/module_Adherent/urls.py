bac47675e57e9633865bde0ee3c8a0adb3b7ce70
Clement <launayclement@laposte.net>
2015-12-07 14:01:03 +0100

    Ajout des premières ébauches de vues

--- siteWeb_solidaire/module_Adherent/urls.py
+++ siteWeb_solidaire/module_Adherent/urls.py
@@ -1,14 +1,14 @@
-#Fichier urls de siteWeb_solidaire
+#Fichier urls de module_Adherent
 
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from module_Adherent import views
+from . import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'siteWeb_solidaire.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^adherents/', include('module_Adherent.urls')),
+    url(r'^$', views.liste_adherent),
+    url(r'^adherent/(?P<id_adherent>\d+)', views.voir_adherent),
 )
--- siteWeb_solidaire/module_Adherent/views.py
+++ siteWeb_solidaire/module_Adherent/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+#Fichier views de module_Adherent
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def liste_adherent(request):
+	text = "Ici on pourra voir la liste des adhérents"
+	
+	return HttpResponse(text)
+
+def voir_adherent(request, id_adherent):
+	text = "Ca, ce sera la fiche de l'adherent n°{0}".format(id_adherent)
+	return HttpResponse(text)
