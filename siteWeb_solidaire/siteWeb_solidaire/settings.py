dbe4fa10d410a613926baa4b027a7a3971da388c
iskandar0 <corentin.robin@supelec.fr>
2016-02-03 15:59:46 +0100

    Commit pour que la branche apparaisse sur le graphique

--- siteWeb_solidaire/siteWeb_solidaire/settings.py
+++ siteWeb_solidaire/siteWeb_solidaire/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'module_Adherent',
     'ressourcesAdherent',
     'gestion',
     'services',
